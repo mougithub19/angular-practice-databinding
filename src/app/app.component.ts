30943a375f4205495488eada56970955ae7fa169
mougithub19 <moubusiness18@gmail.com>
2019-05-28 14:46:36 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  message:string="";
+  display(val:string):void{
+    this.message=val
+  }
 }
